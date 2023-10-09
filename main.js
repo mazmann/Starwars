83a8092993ca8d58ee532d09001d4d35bbaa1386
mazmann <sadi.lundberg@hotmail.com>
2023-10-09 19:13:22 +0200

    finished

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-// async function fetchPlanets() { 
-//     let results = await fetch("https://swapi.dev/api/starships/?page=1");
-//     const data = await results.json();
-//       console.log(data);
-//     let count = data.count;
-//     let next = data.next;
-//     let previous = data.previous;
-//       console.log(count);
-//       console.log(next);
-//       console.log(previous);
-  
-//       console.log(data.results[0]);
-//     let name = data.results[0].name;
+async function asyncFetch() {
+    const res = await fetch(`https://swapi.dev/api/starships/`);
+    const data = await res.json();
+    displayResults(data);
+}
 
-    
-//     let planets = data.results;
-//     planets.forEach(item => {
-//       console.log(item.name);
-//       console.log(item.crew);
-//       console.log(item.manufacturer);
-//       console.log(item.cargo_capacity);
-//       console.log(item.hyperdrive_rating);
-//     });  
-//   } 
-//   fetchPlanets();
+function displayResults(data) {
+    const starshipsToShow = data.results.slice(0, 10);
+    let output = "";
+    data.results.forEach((item, index) => {
+        output += `
+            <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                <div class="card">
+                    <h2>${item.name}</h2>
+                    <p>Crew: ${item.crew}</p>
+                    <p>Model: ${item.model}</p>
+                    <p>Class: ${item.starship_class}</p>
+                    <p>Manufacturer: ${item.manufacturer}</p>
+                    <!-- Add more details as needed -->
+                </div>
+            </div>
+        `;
+    });
 
-  
+    const containerDiv = document.getElementById('carousel-inner');
+    containerDiv.innerHTML = output;
+}
+
+asyncFetch();
